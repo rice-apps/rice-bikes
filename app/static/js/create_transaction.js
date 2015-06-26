9a9a250e179477cc837ee8e4709ef1fa5e8b1e0c
Jonathan Wilson <jdw7@rice.edu>
2015-06-25 21:16:37 -0500

    Fixed automatic price calculator in /edit
    Modified create_transaction.js to add/subtract on the price textfield
    on input:checkbox change events.

--- app/static/js/create_transaction.js
+++ app/static/js/create_transaction.js
@@ -2,20 +2,22 @@ $(document).ready(function() {
 
     $('#id_1-price').val(0);
 
-    $('.checkbox').change(function() {
+    $('input:checkbox').change(function() {
 
         // This is explicitly for prices with format ($<integer>)
         raw_text = $(this).closest('.fieldWrapper').text();
+
         text = raw_text.split(/\s+/)[1];
         task_price = parseInt(text.substring(2, text.length-1));
 
-        was_checked = (this.checked);
-        if(!was_checked) {
-            this.checked = true;
+        console.log($(this));
+
+        is_checked = $(this).prop('checked');
+
+        if(is_checked) {
             $('#id_1-price').val(parseInt($('#id_1-price').val()) + task_price);
         }
         else{
-            this.checked = false;
             $('#id_1-price').val(parseInt($('#id_1-price').val()) - task_price);
         }
     });
