be5e34d1ef59c46e4f30b41d3969dc29e9a19c21
Jonathan Wilson <jdw7@rice.edu>
2015-07-19 15:22:08 -0500

    Interactively add PartCategory forms in Create-Transaction

--- app/static/js/create_trans_parts.js
+++ app/static/js/create_trans_parts.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-    submit = $(':submit').clone();
-    $(':submit').remove();
-    fields = $('.fieldWrapper').clone();
-    $('form').append(fields);
-    $('form').append(submit);
+
+    var form_num = 0;
+    $('#add_form').click(function(){
+        console.log(form_num);
+        buttons = $('#buttons').clone(true);
+        $('#buttons').remove();
+        fields = $('#original_fields').clone().prop('id', $('#original_fields').attr('id') + form_num);
+        $('form').append(fields);
+        $('form').append(buttons);
+
+        form_num += 1;
+    });
 });
