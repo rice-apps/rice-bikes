ebae7cf0c0e2a718eec161f7ba273cb8a967a983
Jonathan Wilson <jdw7@rice.edu>
2015-08-25 19:25:32 -0500

    Calculate Cost on Edit page

--- app/static/js/edit.js
+++ app/static/js/edit.js
@@ -28,16 +28,48 @@ $(document).ready(function() {
 
     // change cost based on number
     $(".number").on("change", function(){
-        console.log("Before, " +  $(this).parent().find('.old_number').attr('old_value'));
+        var old_value = $(this).parent().find('.old_number').attr('old_value')
+        console.log("Before, " +  old_value);
 
         var price = $(this).parent().find('.price').val();
-        var cost_change = price * ($(this).val() - $(this).parent().find('.old_number').attr('old_value'));
+
+        var cost_change;
+        if (old_value == 0){
+            cost_change = price * ($(this).val());
+        }
+        else{
+            cost_change = price * ($(this).val() - old_value);
+        }
         var new_cost = parseInt($("#id_cost").val()) + cost_change;
+
         $("#id_cost").val(new_cost);
         $(this).parent().find('.old_number').attr('old_value', $(this).val());
 
         console.log("After, " +  $(this).parent().find('.old_number').attr('old_value'));
 
     });
+
+    $(".price").on("change", function(){
+        var old_value = $(this).parent().find('.old_price').attr('old_value')
+        console.log("Before, " +  old_value);
+
+        var number = $(this).parent().find('.number').val();
+
+        var cost_change;
+        if (old_value == 0){
+            cost_change = number * ($(this).val());
+        }
+        else{
+            cost_change = number * ($(this).val() - old_value);
+        }
+        var new_cost = parseInt($("#id_cost").val()) + cost_change;
+
+        $("#id_cost").val(new_cost);
+        $(this).parent().find('.old_price').attr('old_value', $(this).val());
+
+        console.log("After, " +  $(this).parent().find('.old_price').attr('old_value'));
+
+    });
+
 
 });
